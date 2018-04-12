b9e29bf0b0076c446e0baa02f989d46f148fb3d1
Michael Wellner <michael.wellner@de.ibm.com>
2018-04-12 09:22:22 +0200

    Fixed small issue during shutdown of the application.

--- src/main/java/ch/mobi/pyt/Controller.java
+++ src/main/java/ch/mobi/pyt/Controller.java
@@ -14,6 +14,7 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scala.compat.java8.FutureConverters;
 
 import java.io.Console;
 import java.io.IOException;
@@ -95,7 +96,7 @@ public class Controller {
     public CompletionStage<Done> stop() {
         CompletionStage<Done> done = binding
             .thenCompose(ServerBinding::unbind)
-            .thenApply(any -> system.terminate())
+            .thenComposeAsync(any -> FutureConverters.toJava(system.terminate()))
             .thenApply(any -> Done.getInstance());
 
         done.thenAccept(d -> LOG.info(String.format("Stopped API-Server on http://%s:%s", hostname, port)));
